ffbf62e6dc493464875868622ea9283c78173760
Nils Matic <matic1909@gmail.com>
2023-10-09 15:17:22 +0200

    added soultion for exercise 1.10

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -58,17 +58,24 @@ const Statistics = ({ good, neutral, bad, total, average, positive }) => {
       <h2>statistics</h2>
       {total > 0 ? (
         <>
-          <p>good {good}</p>
-          <p>neutral {neutral}</p>
-          <p>bad {bad}</p>
-          <p>all {total}</p>
-          <p>average {average}</p>
-          <p>positive {positive}%</p>
+          <StatisticLine text="good" value={good} />
+          <StatisticLine text="neutral" value={neutral} />
+          <StatisticLine text="bad" value={bad} />
+          <StatisticLine text="all" value={total} />
+          <StatisticLine text="average" value={average} />
+          <StatisticLine text="positive" value={positive} />
         </>
-      ) : (
-        <p>No feeback given</p>
-      )}
+      ) : null}
     </>
+  );
+};
+
+const StatisticLine = ({ text, value }) => {
+  return (
+    <p>
+      {text} {value}
+      {text === "positive" ? "%" : ""}
+    </p>
   );
 };
 
