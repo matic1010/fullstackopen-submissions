3d3003abb2d1452af107d3d0608e82f8ed0a8ae9
Nils Matic <matic1909@gmail.com>
2023-10-09 19:02:55 +0200

    added solution for exercise 2.8

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,13 +1,20 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "12345-333-12" },
+  ]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
   const names = persons.map((person) => person.name);
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
+  };
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
   };
 
   const addPerson = (event) => {
@@ -19,10 +26,12 @@ const App = () => {
     }
     const personToAdd = {
       name: newName,
+      number: newNumber,
     };
 
     setPersons(persons.concat(personToAdd));
     setNewName("");
+    setNewNumber("");
   };
 
   return (
@@ -33,12 +42,17 @@ const App = () => {
           name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       {persons.map((person) => (
-        <p key={person.name}>{person.name}</p>
+        <p key={person.name}>
+          {person.name}: {person.number}
+        </p>
       ))}
     </div>
   );
