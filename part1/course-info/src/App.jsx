e9695bbca17df78c6d04260f72ff35101d009c28
Nils Matic <matic1909@gmail.com>
2023-10-09 13:05:10 +0200

    added solution for exercise 1.2

--- part1/course-info/src/App.jsx
+++ part1/course-info/src/App.jsx
@@ -7,12 +7,13 @@ const App = () => {
   const part3 = "State of a component";
   const exercises3 = 14;
 
+  const parts = [part1, part2, part3];
+  const exercises = [exercises1, exercises2, exercises3];
+
   return (
     <div>
       <Header course={course} />
-      <Content part={part1} exercises={exercises1} />
-      <Content part={part2} exercises={exercises2} />
-      <Content part={part3} exercises={exercises3} />
+      <Content parts={parts} exercises={exercises} />
       <Total
         exercises1={exercises1}
         exercises2={exercises2}
@@ -27,6 +28,16 @@ const Header = (props) => {
 };
 
 const Content = (props) => {
+  return (
+    <div>
+      <Part part={props.parts[0]} exercises={props.exercises[0]} />
+      <Part part={props.parts[1]} exercises={props.exercises[1]} />
+      <Part part={props.parts[2]} exercises={props.exercises[2]} />
+    </div>
+  );
+};
+
+const Part = (props) => {
   return (
     <p>
       {props.part} {props.exercises}
